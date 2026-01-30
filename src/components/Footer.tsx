ad19438b643cd8095cc1213790a9f5b5cb4b17a5
ciobanut <iofesty@gmail.com>
2026-01-30 13:50:16 +0200

    fix: the slider Swiper to have normal width in the parent container, added few mock slides images

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,8 @@
 
 export default function Footer() {
   return (
-    <footer className="text-secondary ml-[var(--sidebar-width-left)] mr-[var(--sidebar-width-right)] min-w-[calc(100vw-var(--sidebar-collapsed-width))] sm:min-w-fit">
-      <div className="px-4 py-6 flex flex-col md:flex-row items-center justify-between gap-3">
+    <footer className="text-secondary">
         <p className="text-sm">© {new Date().getFullYear()} HYPE. Alle Rechte vorbehalten.</p>
-      </div>
     </footer>
   );
 }
